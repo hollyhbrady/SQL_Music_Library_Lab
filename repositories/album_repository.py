c8e9978aabd90ae1cb59da8d9707209bdd505983
hollyhbrady <hollyhbrady@gmail.com>
2021-04-20 20:51:14 +0100

    save(album) working

--- repositories/album_repository.py
+++ repositories/album_repository.py
@@ -6,7 +6,12 @@ from models.artist import Artist
 import repositories.artist_repository as artist_repository
 
 def save(album):
-    pass
+    sql = "INSERT INTO albums (title, genre, artist_id) VALUES (%s, %s, %s) RETURNING *"
+    values = [album.title, album.genre, album.artist.id]
+    results = run_sql(sql, values)
+    id = results[0]['id']
+    album.id = id
+    return album
 
 def delete_all():
     pass
