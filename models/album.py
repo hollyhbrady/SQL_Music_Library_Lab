51d18d25e5ee026d14c4a503106c148aab5fbbfc
hollyhbrady <hollyhbrady@gmail.com>
2021-04-20 18:24:29 +0100

    Set up classes for Artist and Album

---
+++ models/album.py
@@ -0,0 +1,5 @@
+class Album:
+    
+    def __init__(self, name, id):
+        self.name = name
+        self.id = id
---
+++ models/artist.py
@@ -0,0 +1,7 @@
+class Artist:
+    
+    def __init__(self, title, genre, artist, id):
+        self.title = title
+        self.genre = genre
+        self.artist = artist
+        self.id = id
